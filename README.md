cd69913e1b1dd5bff510a5c7f9d5a501caa6c1db
HaySsor <hayss007@gmail.com>
2022-04-25 12:44:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# winda-ftgrg7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/winda-ftgrg7)
---
+++ index.js
@@ -0,0 +1,22 @@
+import './style.css';
+
+const directions = {
+  top: '🔼',
+  bottom: '🔽',
+};
+const direction = document.querySelector('.direction');
+const elevator = document.querySelector('.elevator');
+let currentScroll = 0;
+
+elevator.addEventListener('scroll', () => {
+  let positionFromTop = elevator.scrollTop;
+  let floorNumber = Math.floor(10.8 - positionFromTop / 150);
+
+  if (currentScroll > elevator.scrollTop) {
+    direction.textContent = `Kierunek: ${directions.top}  ${floorNumber}`;
+  } else {
+    direction.textContent = `Kierunek: ${directions.bottom}  ${floorNumber}`;
+  }
+
+  currentScroll = elevator.scrollTop;
+});
